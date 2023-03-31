f81e0393f9c22433b81a738ab808ec055407d35d
= <luigi.naudi@gmail.com>
2023-04-01 00:59:33 +0200

    API integration start; error fixed, title being printed

--- app/src/main/java/com/example/mobileassignment/MainActivity.java
+++ app/src/main/java/com/example/mobileassignment/MainActivity.java
@@ -14,24 +14,9 @@ import androidx.navigation.ui.NavigationUI;
 
 import com.example.mobileassignment.databinding.ActivityMainBinding;
 
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
-
-
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    public static String BASE_URL = "https://api.themoviedb.org";
-    public static int PAGE = 1;
-    public static String API_KEY = "1bde390d4ef9fe09e434ab3569042f63";
-    public static String LANGUAGE = "en-US";
-    public static String CATEGORY = "popular";
-    private TextView movieTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,31 +32,5 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
-        movieTextView = (TextView)findViewById(R.id.movies_discover);
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-
-        ApiInterface apiInterface = retrofit.create(ApiInterface.class);
-        //build URL using constants
-        Call<MovieResults> call = apiInterface.getMovies(CATEGORY,API_KEY,LANGUAGE,PAGE);
-        call.enqueue(new Callback<MovieResults>() {
-            @Override
-            public void onResponse(Call<MovieResults> call, Response<MovieResults> response) {
-                MovieResults results = response.body();
-                List<MovieResults.ResultsBean> ListOfMovies = results.getResults();
-                //getting the first movie in the list
-                MovieResults.ResultsBean firstMovie = ListOfMovies.get(0);
-                movieTextView.setText(firstMovie.getTitle());
-            }
-
-            @Override
-            public void onFailure(Call<MovieResults> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
     }
 }
--- app/src/main/java/com/example/mobileassignment/ui/Discover/DiscoverViewModel.java
+++ app/src/main/java/com/example/mobileassignment/ui/Discover/DiscoverViewModel.java
@@ -1,19 +1,62 @@
 package com.example.mobileassignment.ui.Discover;
 
+import android.view.LayoutInflater;
+import android.widget.TextView;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.mobileassignment.API.ApiInterface;
+import com.example.mobileassignment.API.MovieResults;
+import com.example.mobileassignment.R;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class DiscoverViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
+    public static String BASE_URL = "https://api.themoviedb.org";
+    public static int PAGE = 1;
+    public static String API_KEY = "1bde390d4ef9fe09e434ab3569042f63";
+    public static String LANGUAGE = "en-US";
+    public static String CATEGORY = "popular";
+
+    private MutableLiveData<String> movieTitle = new MutableLiveData<>();
 
     public DiscoverViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is discover fragment");
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        ApiInterface apiInterface = retrofit.create(ApiInterface.class);
+        //build URL using constants
+        Call<MovieResults> call = apiInterface.getMovies(CATEGORY,API_KEY,LANGUAGE,PAGE);
+        call.enqueue(new Callback<MovieResults>() {
+            @Override
+            public void onResponse(Call<MovieResults> call, Response<MovieResults> response) {
+                MovieResults results = response.body();
+                List<MovieResults.ResultsBean> ListOfMovies = results.getResults();
+                //getting the first movie in the list
+                MovieResults.ResultsBean firstMovie = ListOfMovies.get(0);
+                movieTitle.setValue(firstMovie.getTitle());
+            }
+
+            @Override
+            public void onFailure(Call<MovieResults> call, Throwable t) {
+                t.printStackTrace();
+            }
+        });
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public LiveData<String> getMovieTitle() {
+        return movieTitle;
     }
 }
