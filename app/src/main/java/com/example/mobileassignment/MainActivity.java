c1b943bd9cb8e1032806551388cfca363e3c6cf2
IsmaelMagro21 <ismael.magro.21@um.edu.mt>
2023-04-15 15:21:03 +0200

    Setting up of MovieDetails Activity
    To do:
    //get the Intent
    //get the Serializable object Movie(id) which has been clicked on
    //populate the Views with the appropriate Data

--- app/src/main/java/com/example/mobileassignment/MainActivity.java
+++ app/src/main/java/com/example/mobileassignment/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_dashboard, R.id.navigation_home, R.id.navigation_notifications).build();
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_dashboard, R.id.navigation_profile).build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
--- app/src/main/java/com/example/mobileassignment/ui/Profile/ProfileAdapter.java
+++ app/src/main/java/com/example/mobileassignment/ui/Profile/ProfileAdapter.java
@@ -1,13 +1,11 @@
 package com.example.mobileassignment.ui.Profile;
 
 import android.content.Context;
-import android.graphics.Movie;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,5 +14,47 @@ import com.example.mobileassignment.R;
 import java.util.List;
 
 public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ViewHolder> {
+    private List<String> items;
+    public ProfileAdapter(List<String> items) {
+        this.items = items;
+    }
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View itemView = inflater.inflate(R.layout.profilecard, parent, false);
+        return new ViewHolder(itemView);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position)
+    {
+        String item = items.get(position);
+    }
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder {
 
+        public ImageView posterImageView;
+
+        public ViewHolder(final View itemView) {
+            super(itemView);
+            posterImageView = (ImageView)
+                    itemView.findViewById(R.id.movie_poster);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    itemOnClick();
+                }
+            });
+        }
+
+        public void itemOnClick() {
+
+        }
+    }
 }
+
